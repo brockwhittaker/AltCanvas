c2f73bc1b9f1f0f2ab953482c9ba727e5a25da58
brockwhittaker <bjwhitta@asu.edu>
2015-10-23 09:53:39 -0700

    AltCanvas Initial Commit
    This is the initial commit of the first working prototype of AltCanvas.

---
+++ js/canvas.js
@@ -0,0 +1,146 @@
+function Canvas () {
+  this.style = {
+    brushSize: 1,
+    brushColor: [0,0,0]
+  };
+}
+
+Canvas.prototype = {
+  asHex: function (value, bytes) {
+    var result = [];
+
+    while (bytes > 0) {
+      bytes--;
+      result.push(String.fromCharCode(value & 255));
+      value >>= 8;
+    }
+
+    return result.join('');
+  },
+
+  addData: function (pixelGrid) {
+    this.data = pixelGrid;
+
+    return this;
+  },
+
+  formatData: function () {
+    var padding = "\x00".repeat(this.padding),
+        pixel,
+        formattedData = "";
+
+    for (var x = this.width - 1; x >= 0; x--) {
+      for (var y = 0; y < this.height; y++) {
+        pixel = this.data[x][y];
+        formattedData += (String.fromCharCode(pixel[2]) || 255);
+        formattedData += (String.fromCharCode(pixel[1]) || 255);
+        formattedData += (String.fromCharCode(pixel[0]) || 255);
+      }
+    }
+
+    this.formattedData = formattedData;
+
+    return this;
+  },
+
+  initData: function () {
+    this.data = new Array(this.width);
+
+    for (var x = 0; x < this.width; x++) {
+      this.data[x] = [];
+      for (var y = 0; y < this.height; y++) {
+        this.data[x][y] = [255,255,255];
+      }
+    }
+  },
+
+  init: function (div, width, height) {
+    this.container = div;
+    this.width = width;
+    this.height = height;
+    this.padding = (4 - (this.width * 3) % 4) % 4;
+    this.dataSize = (this.width * 3 + this.padding) * this.height;
+    this.fileSize = 54 + this.dataSize;
+
+    this.initData();
+
+    this.container.width = width;
+    this.container.height = height;
+
+    return this;
+  },
+
+  createFile: function () {
+    var file = ('BM' +                // "Magic Number"
+      this.asHex(this.fileSize, 4) +  // size in bytes
+      '\x00\x00' +                    // reserved
+      '\x00\x00' +                    // reserved
+      '\x36\x00\x00\x00' +            // 54 byte offset for data
+      '\x28\x00\x00\x00' +            // number of remaining bytes in header (40 bytes)
+      this.asHex(this.width, 4) +     // the width
+      this.asHex(this.height, 4) +    // the height
+      '\x01\x00' +                    // the number of color planes (1)
+      '\x18\x00' +                    // 24 bits / pixel (3 channels)
+      '\x00\x00\x00\x00' +            // No compression (0)
+      this.asHex(this.dataSize, 4) +  // size of the BMP data (bytes)
+      '\x13\x0B\x00\x00' +            // 2835 pixels/meter - horizontal resolution
+      '\x13\x0B\x00\x00' +            // 2835 pixels/meter - the vertical resolution
+      '\x00\x00\x00\x00' +            // Number of colors in the palette (keep 0 for 24-bit)
+      '\x00\x00\x00\x00' +            // 0 important colors (means all colors are important)
+      this.formattedData              // the stringified array data
+    );
+
+    this.file = btoa(file);
+
+    return this;
+  },
+
+  displayImage: function (data) {
+    this.executeTime = {
+      start: new Date() * 1,
+      end: null,
+      total: null
+    };
+
+    if (data) {
+      this.addData(data);
+    }
+
+    this.formatData()
+        .createFile();
+
+    this.container.src = 'data:image/bmp;base64,' + this.file;
+
+    this.executeTime.end = new Date() * 1;
+    this.executeTime.total = this.executeTime.end - this.executeTime.start;
+
+    return this;
+  },
+
+  /* --- drawing extensions here --- */
+
+  drawPixel: function (x, y) {
+    var b = this.style.brushSize;
+
+    for (var i = 0; i < b; i++) {
+      for (var j = 0; j < b; j++) {
+        try {
+          this.data[y + i - Math.round(b / 2)][x + j - Math.round(b / 2)] = this.style.brushColor;
+          // it's y then x because each y has a container of x...
+        } catch (e) {
+          // silence..
+        }
+      }
+    }
+
+    return this;
+  },
+
+  drawRectangle: function (x1, x2, y1, y2) {
+    for (var i = Math.min(x1, x2); i < Math.max(x1, x2); i++) {
+      for (var j = Math.min(y1, y2); j < Math.max(y1, y2); j++) {
+        this.data[j][i] = this.style.brushColor;
+      }
+    }
+  }
+};
---
+++ js/main.js
@@ -0,0 +1,81 @@
+var canvas = new Canvas(),
+    canvasElem = document.getElementById('canvas');
+
+function ClickEvents () {
+  this.drawRectangle = false;
+  this.mouseIsDown = false;
+}
+
+ClickEvents.prototype = {
+  events: function (div, canvas) {
+    var $this = this;
+    div.onclick = function (e) {
+      if ($this.drawRectangle) {
+        $this.mouseDown = !$this.mouseDown;
+        if ($this.mouseDown) {
+          $this.offset = {
+            x: e.offsetX,
+            y: e.offsetY
+          };
+        } else {
+          canvas.drawRectangle($this.offset.x, e.offsetX, $this.offset.y, e.offsetY);
+          canvas.displayImage();
+          $("#time_taken").html(canvas.executeTime.total + "ms");
+          console.log(canvas.executeTime);
+          $this.offset = null;
+        }
+      }
+    };
+    div.onmousemove = function (e) {
+      if (!$this.drawRectangle && $this.mouseIsDown) {
+        canvas.drawPixel(e.offsetX, e.offsetY);
+        canvas.displayImage();
+        $("#time_taken").html(canvas.executeTime.total + "ms");
+        console.log(canvas.executeTime);
+      }
+    };
+    div.onmousedown = function (e) {
+      $this.mouseIsDown = true;
+    };
+    div.onmouseup = function (e) {
+      $this.mouseIsDown = false;
+    };
+    div.ondragstart = function (e) {
+      return false;
+    };
+  }
+};
+
+// --- init canvas and add settings --- //
+canvas.init(canvasElem, 300, 300);
+
+canvas.style.brushSize = $("#brush_size")[0].defaultValue = 20;
+canvas.style.brushColor = [230,230,230];
+// ------------------------------------ //
+
+// --- init doc events --- //
+var DocEvents = new ClickEvents();
+DocEvents.events(canvasElem, canvas);
+
+$(".ind-color").click(function (e) {
+  var color = e.target.className.split(" ")[1]; // get the color name (second class)
+  canvas.style.brushColor = JSON.parse(document.getElementsByClassName(color)[0].getAttribute('color')); // get color attr value (array) and apply
+});
+
+$("#brush_size").on("change", function () {
+  canvas.style.brushSize = parseInt($(this).val(), 10);
+}); // change the canvas brush size on slider change
+
+
+$(".option").click(function (e) {
+  $(".option").each(function () {
+    $(this).removeClass("selected"); // orange -> grey
+  });
+  $(this).addClass("selected"); // grey -> orange
+  if ($(this).hasClass("rectangle")) {
+    DocEvents.drawRectangle = true;
+  } else {
+    DocEvents.drawRectangle = false;
+  }
+});
+// ----------------------- //
